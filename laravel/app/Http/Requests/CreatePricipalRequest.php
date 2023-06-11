b84caa08ea9a8d354d1b4a35703b149684ec542f
Antoni Seba <antoniwoj@o2.pl>
2023-06-11 13:21:06 +0200

    Add customer panel

--- laravel/app/Http/Requests/CreatePricipalRequest.php
+++ laravel/app/Http/Requests/CreatePricipalRequest.php
@@ -23,15 +23,15 @@ class CreatePricipalRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string',
-            'description' => 'required|string',
-            'nip' => 'required|string',
-            'address' => 'required|string',
-            'email' => 'required|string',
-            'accounting_email' => 'required|string',
-            'website' => 'required|string',
-            'password' => 'required|string',
-            'phone' => 'required|string',
+            'name' => ['required', 'string'],
+            'description' => ['required', 'string'],
+            'nip' => ['required', 'string'],
+            'address' => ['required', 'string'],
+            'email' => ['required', 'string', 'email', 'unique:users,email'],
+            'accounting_email' => ['required', 'string'],
+            'website' => ['required', 'string'],
+            'password' => ['required', 'string'],
+            'phone' => ['required', 'string'],
         ];
     }
 }
